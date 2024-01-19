652d91894d8b440178e1b6e3046834b6a2fcc72c
Anne LoVerso <anne.loverso@gmail.com>
2024-01-19 11:21:44 -0500

    anne and anju

--- src/YahtzeeGame.test.ts
+++ src/YahtzeeGame.test.ts
@@ -1,7 +1,19 @@
 import { YahtzeeGame } from './YahtzeeGame'
 
 describe('Yahtzee Game', () => {
+
   it('starts with score 0', () => {
     expect(new YahtzeeGame().score()).toEqual(0)
   })
+
+  it('scores a roll as Chance and increments the score by the sum of 5 dice rolled', () => {
+    const game = new YahtzeeGame()
+    game.roll([1,2,3,4,5])
+    expect(game.score()).toEqual(15)
+  })
+
+  it('scores a roll as 4 of a kind and increments the score by 40', () => {
+    
+  })
+
 })
--- src/YahtzeeGame.ts
+++ src/YahtzeeGame.ts
@@ -1,12 +1,23 @@
 export class YahtzeeGame {
+
+  runningScore = 0;
+
   constructor() {
   }
 
-  roll (): void {
-
+  /*
+  a Roll is the array of 5 dice in a round
+  a Die/Dice is one of the 5 elements of the Roll
+  */
+  roll (dice: number[]): void {
+    this.runningScore += sum(dice);
   }
 
   score (): number {
-    return 0
+    return this.runningScore
   }
+}
+
+const sum = (dice: number[]): number => {
+ return dice.reduce((acc, curr) => acc + curr, 0)
 }
