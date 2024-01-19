8788da29f7b0883dbdc723fc4e6aeef40d3e246d
Noah Marcus <noah@innovation.nj.gov>
2024-01-19 11:55:08 -0500

    round 2 (on sam's behalf)

--- src/YahtzeeGame.test.ts
+++ src/YahtzeeGame.test.ts
@@ -5,10 +5,42 @@ describe("Yahtzee Game", () => {
     expect(new YahtzeeGame().score()).toEqual(0);
   });
 
-  it("correctly calculates the score for a yahtzee", () => {
+  it("correctly calculates the score for a yahtzee of 6s on first round", () => {
     const game = new YahtzeeGame();
 
     game.roll([6, 6, 6, 6, 6]);
     expect(game.score()).toEqual(50);
+  });
+
+  it("correctly calculates the score for a yahtzee of 1s on first round", () => {
+    const game = new YahtzeeGame();
+
+    game.roll([1, 1, 1, 1, 1]);
+    expect(game.score()).toEqual(50);
+  });
+
+  it("correctly calculates the score for a large straight on first round", () => {
+    const game = new YahtzeeGame();
+
+    game.roll([1, 4, 3, 2, 5]);
+    expect(game.score()).toEqual(40);
+  });
+
+  it("correctly calculates the score for a short straight on first round", () => {
+    const game1 = new YahtzeeGame();
+
+    game1.roll([1, 4, 3, 2, 2]);
+    expect(game1.score()).toEqual(30);
+
+    const game2 = new YahtzeeGame();
+    game2.roll([2, 5, 4, 3, 5]);
+    expect(game2.score()).toEqual(30);
+  });
+
+  it("correctly calculates the score for a 4 of a kind on first round", () => {
+    const game = new YahtzeeGame();
+
+    game.roll([4, 4, 4, 4, 1]);
+    expect(game.score()).toEqual(40);
   });
 });
--- src/YahtzeeGame.ts
+++ src/YahtzeeGame.ts
@@ -1,12 +1,57 @@
 type Round = {};
 
 export class YahtzeeGame {
-  runningScore: number;
+  private runningScore: number;
   constructor() {
     this.runningScore = 0;
   }
 
-  roll(round: number[]): void {}
+  roll(round: number[]): void {
+    if (this.isYahtzee(round)) {
+      this.runningScore += 50;
+    }
+
+    if (this.isLargeStraight(round)) {
+      this.runningScore += 40;
+    }
+
+    if (this.isShortStraight(round)) {
+      this.runningScore += 30;
+    }
+  }
+
+  private isYahtzee(round: number[]): boolean {
+    return round.every((num) => {
+      return num === round[0];
+    });
+  }
+
+  private isLargeStraight(round: number[]): boolean {
+    return (
+      round.includes(1) &&
+      round.includes(2) &&
+      round.includes(3) &&
+      round.includes(4) &&
+      round.includes(5)
+    );
+  }
+
+  private isShortStraight(round: number[]): boolean {
+    return (
+      (round.includes(1) &&
+        round.includes(2) &&
+        round.includes(3) &&
+        round.includes(4)) ||
+      (round.includes(2) &&
+        round.includes(3) &&
+        round.includes(4) &&
+        round.includes(5))
+    );
+  }
+
+  private isFourOfAKind(round: number[]): boolean {
+    return false;
+  }
 
   score(): number {
     return this.runningScore;
