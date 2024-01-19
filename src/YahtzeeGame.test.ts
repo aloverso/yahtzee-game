4f40af189019f4a47276c5ec3ffebd03d0a719b6
Case Walker <case.b.walker@gmail.com>
2024-01-19 13:39:38 -0500

    Add dice sums and stuff

--- src/YahtzeeGame.test.ts
+++ src/YahtzeeGame.test.ts
@@ -1,8 +1,13 @@
 import { YahtzeeGame } from "./YahtzeeGame";
 
 describe("Yahtzee Game", () => {
+  let game;
+
+  beforeEach(() => {
+    game = new YahtzeeGame()
+  })
   it("starts with score 0", () => {
-    expect(new YahtzeeGame().score()).toEqual(0);
+    expect(game.score()).toEqual(0);
   });
 
   // it("roll ones and score adds the sum of 1s", () => {
@@ -12,13 +17,12 @@ describe("Yahtzee Game", () => {
   // });
 
   it("scores yahtzee properly on round 1", () => {
-    const game = new YahtzeeGame();
+
     game.roll([1, 1, 1, 1, 1]);
     expect(game.score()).toEqual(50);
   });
 
   it("doesn't score a yahtzee if a yahtzee wasn't rolled", () => {
-    const game = new YahtzeeGame();
     game.roll([1, 3, 1, 1, 1]);
     expect(game.score()).not.toEqual(50);
   });
@@ -49,11 +53,18 @@ describe("Yahtzee Game", () => {
 
   // REMINDER: TEST STRAIGHTS THAT CAN BE BIG OR SMALL DEPENDING ON WHATS TAKEN
 
-  it("scores full house properly on round 1", () => {
-    const game1 = new YahtzeeGame();
-    game1.roll([1,1,2,2,2]);
-    expect(game1.score()).toEqual(25);
-
+  it.each([
+    [[1,1,2,2,2]],
+    [[3,5,5,3,5]]
+  ])
+  ("scores full house properly on round 1", (diceValues) => {
+    game.roll(diceValues);
+    expect(game.score()).toEqual(25);
   });
+
+  it("scores four of kind properly on round 1", () => {
+    game.roll([3,3,3,3,1]);
+    expect(game.score()).toEqual(13);
+  })
   // testing
 });
--- src/YahtzeeGame.ts
+++ src/YahtzeeGame.ts
@@ -33,43 +33,67 @@ export class YahtzeeGame {
     };
   }
 
-  roll(round: number[]): void {
-    if (this.isYahtzee(round)) {
+  roll(diceValues: number[]): void {
+    if (this.isYahtzee(diceValues)) {
       this.ScoreCard.yahtzee = 50;
     }
 
-    if (this.isLargeStraight(round)) {
+    if (this.isLargeStraight(diceValues)) {
       this.ScoreCard.largeStraight = 40;
+    } else if (this.isSmallStraight(diceValues)) {
+      this.ScoreCard.smallStraight = 30;
     }
 
-    if (this.isSmallStraight(round)) {
-      this.ScoreCard.smallStraight = 30;
+    if (this.isFullHouse(diceValues)) {
+      this.ScoreCard.fullHouse = 25;
+    }
+
+    if(this.isSumOfDiceValuesCategory(diceValues)) {
+      this.ScoreCard.fourOfAKind = this.sumOfDice(diceValues);
     }
   }
 
-  private isYahtzee(round: number[]): boolean {
-    return round.every((num) => {
-      return num === round[0];
+  private isYahtzee(diveValues: number[]): boolean {
+    return diveValues.every((num) => {
+      return num === diveValues[0];
     });
   }
 
-  private isLargeStraight(round: number[]): boolean {
-    return (round.includes(2) && round.includes(3) && round.includes(4) && round.includes(5))
-        && (round.includes(1) || round.includes(6))
+  private isLargeStraight(diceValues: number[]): boolean {
+    return (diceValues.includes(2) && diceValues.includes(3) && diceValues.includes(4) && diceValues.includes(5))
+        && (diceValues.includes(1) || diceValues.includes(6))
   }
 
-  private isSmallStraight(round: number[]): boolean {
-    return (round.includes(3) && round.includes(4))
+  private isSmallStraight(diceValues: number[]): boolean {
+    return (diceValues.includes(3) && diceValues.includes(4))
         &&
         (
-                (round.includes(1) && round.includes(2))
-            ||  (round.includes(2) && round.includes(5))
-            ||  (round.includes(5) && round.includes(6))
+                (diceValues.includes(1) && diceValues.includes(2))
+            ||  (diceValues.includes(2) && diceValues.includes(5))
+            ||  (diceValues.includes(5) && diceValues.includes(6))
         )
   }
 
-  private isFullHouse(round: number[]): boolean {
-    return false
+  private getDiceFaceCounts(diceValues: number[]): number[]{
+    const diceFaceValues = [-1, 0, 0, 0, 0, 0, 0 ]
+    diceValues.forEach((dice) => {
+      diceFaceValues[dice] += 1
+    })
+    return diceFaceValues
+  }
+
+  private isFullHouse(diceValues: number[]): boolean {
+    const faceValues = this.getDiceFaceCounts(diceValues)
+    return faceValues.includes(3) && faceValues.includes(2)
+  }
+
+  private isSumOfDiceValuesCategory(diceValues: number[]): boolean {
+    const faceValues = this.getDiceFaceCounts(diceValues)
+    return faceValues.includes(4)
+  }
+
+  private sumOfDice(diceValues: number[]): number {
+    return diceValues.reduce((val, acc) => val + acc);
   }
 
   score(): number {
