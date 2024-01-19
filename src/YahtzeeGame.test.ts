6c1bea45ad74701d0eba7a5193568f298fa4cee2
Anne LoVerso <anne.loverso@gmail.com>
2024-01-19 16:26:48 -0500

    define domain

--- src/YahtzeeGame.test.ts
+++ src/YahtzeeGame.test.ts
@@ -1,12 +1,25 @@
-import { YahtzeeGame } from './YahtzeeGame'
+import {CategoryScorer, Chooser, YahtzeeGame} from './YahtzeeGame'
 
 describe('Yahtzee Game', () => {
+
+  let chooser: Chooser;
+  let categoryScorer: CategoryScorer;
+
+  beforeEach(() => {
+    chooser = {
+      choose: jest.fn()
+    }
+    categoryScorer = {
+      score: jest.fn()
+    }
+  })
+
   it('starts with score 0', () => {
-    expect(new YahtzeeGame().score()).toEqual(0)
+    expect(new YahtzeeGame(categoryScorer, chooser).score()).toEqual(0)
   })
 
   it('adds the sum of dice rolled to score each round', () => {
-    const game = new YahtzeeGame();
+    const game = new YahtzeeGame(categoryScorer, chooser);
     game.roll([1,1,1,1,1])
     expect(game.score()).toEqual(5)
     game.roll([1,1,1,1,2])
@@ -14,12 +27,16 @@ describe('Yahtzee Game', () => {
   })
 
   it('ends the game after thirteen rounds', () => {
-    const game = new YahtzeeGame();
+    const game = new YahtzeeGame(categoryScorer, chooser);
     for (let i = 0; i < 13; i++) {
       game.roll([1,1,1,1,1])
     }
     expect(game.score()).toEqual(5*13)
     game.roll([1,1,1,1,1])
     expect(game.score()).toEqual(5*13)
+  })
+
+  it('uses a chooser and a categoryScorer to choose and score a category', () => {
+    expect(true).toEqual(true)
   })
 })
--- src/YahtzeeGame.ts
+++ src/YahtzeeGame.ts
@@ -2,9 +2,13 @@ export class YahtzeeGame {
 
   runningScore: number;
   round: number;
+  categoryScorer: CategoryScorer;
+  chooser: Chooser;
 
-  constructor() {
+  constructor(categoryScorer: CategoryScorer, chooser: Chooser) {
     this.round = 1;
+    this.chooser = chooser
+    this.categoryScorer = categoryScorer
     this.runningScore = 0;
   }
 
@@ -24,4 +28,32 @@ export class YahtzeeGame {
 
 const sum = (roll: number[]): number => {
   return roll.reduce((acc, cur) => acc+cur, 0)
+}
+
+type CategoryScores = {
+  ones: number;
+  twos: number;
+}
+
+export interface Chooser {
+  choose: (categories: CategoryScores) => number
+}
+
+export interface CategoryScorer {
+  score: (roll: number[]) => CategoryScores
+}
+
+class YahtzeeChooser implements Chooser {
+  choose(categories): number {
+    return 0
+  }
+}
+
+class YahtzeeCategoryScorer implements CategoryScorer {
+  score(roll): CategoryScores {
+    return {
+      ones: 0,
+      twos: 0
+    }
+  }
 }
