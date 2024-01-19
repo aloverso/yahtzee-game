fa67fe3f6dadbcbc41036e3430c3fc1c6f36db1d
ashlsun <janniesun00@gmail.com>
2024-01-19 12:37:45 -0500

    round three

--- src/YahtzeeGame.ts
+++ src/YahtzeeGame.ts
@@ -18,10 +18,11 @@ interface ScoreSheet {
 }
 
 export class YahtzeeGame {
-  private scoreSheet: ScoreSheet;
+  private finalScoreSheet: ScoreSheet;
+  private tempScoreSheet: ScoreSheet;
 
   constructor() {
-    this.scoreSheet = {
+    this.finalScoreSheet = {
       ones: null,
       twos: null,
       threes: null,
@@ -86,44 +87,84 @@ export class YahtzeeGame {
     return false;
   }
 
+  private updateFinalScoreSheet() {
+    let max = 0;
+    const maxCategory = null;
+    for (const [key, value] of Object.entries(this.tempScoreSheet)) {
+      if (value !== null) {
+        if (value > max) {
+          max = value;
+        }
+      }
+    }
+  }
+
   roll(dice: number[]): void {
+    this.finalScoreSheet = {
+      ones: 0,
+      twos: 0,
+      threes: 0,
+      fours: 0,
+      fives: 0,
+      sixes: 0,
+      threeOfAKind: 0,
+      fourOfAKind: 0,
+      fullHouse: 0,
+      smallStraight: 0,
+      largeStraight: 0,
+      yahtzee: 0,
+      chance: 0,
+    };
+
     switch (true) {
-      case this.onesTest(dice):
-        break;
-      case this.twosTest(dice):
-        break;
-      case this.threesTest(dice):
-        break;
-      case this.foursTest(dice):
-        break;
-      case this.fivesTest(dice):
-        this.scoreSheet.fives = 10;
-        break;
-      case this.sixesTest(dice):
-        break;
-      case this.threeOfAKindTest(dice):
-        break;
-      case this.fourOfAKindTest(dice):
-        this.scoreSheet.fourOfAKind = this.sum(dice);
-        break;
-      case this.fullHouseTest(dice):
-        break;
-      case this.smallStraightTest(dice):
+      case this.yahtzeeTest(dice):
+        this.tempScoreSheet.yahtzee = 50;
         break;
       case this.largeStraightTest(dice):
+        this.tempScoreSheet.yahtzee = 40;
         break;
-      case this.yahtzeeTest(dice):
-        break;
-      default:
-        this.scoreSheet.chance = this.sum(dice);
+      case this.smallStraightTest(dice):
+        this.tempScoreSheet.yahtzee = 30;
         break;
     }
+    if (this.onesTest(dice)) {
+      this.tempScoreSheet.ones = 0;
+    }
+    if (this.twosTest(dice)) {
+      this.tempScoreSheet.twos = 0;
+    }
+    if (this.threesTest(dice)) {
+      this.tempScoreSheet.threes = 0;
+    }
+    if (this.foursTest(dice)) {
+      this.tempScoreSheet.fours = 0;
+    }
+    if (this.fivesTest(dice)) {
+      this.tempScoreSheet.fives = 10;
+    }
+    if (this.sixesTest(dice)) {
+      this.tempScoreSheet.sixes = 0;
+    }
+    if (this.threeOfAKindTest(dice)) {
+      this.tempScoreSheet.threeOfAKind = 0;
+    }
+    if (this.fourOfAKindTest(dice)) {
+      this.tempScoreSheet.fourOfAKind = this.sum(dice);
+    }
+    if (this.fullHouseTest(dice)) {
+      this.tempScoreSheet.fullHouse = 0;
+    }
+
+    this.tempScoreSheet.chance = this.sum(dice);
+
+    console.log(this.tempScoreSheet);
+    console.log(this.finalScoreSheet);
   }
 
   score(): number {
     let score = 0;
 
-    for (const [key, value] of Object.entries(this.scoreSheet)) {
+    for (const [key, value] of Object.entries(this.finalScoreSheet)) {
       if (value !== null) {
         score += value;
       }
