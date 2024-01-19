79e7d321b27436b1f1b3d98381d9045f6da38fca
Anne LoVerso <anne.loverso@gmail.com>
2024-01-19 16:12:23 -0500

    initial game with rounds

--- src/YahtzeeGame.test.ts
+++ src/YahtzeeGame.test.ts
@@ -4,4 +4,22 @@ describe('Yahtzee Game', () => {
   it('starts with score 0', () => {
     expect(new YahtzeeGame().score()).toEqual(0)
   })
+
+  it('adds the sum of dice rolled to score each round', () => {
+    const game = new YahtzeeGame();
+    game.roll([1,1,1,1,1])
+    expect(game.score()).toEqual(5)
+    game.roll([1,1,1,1,2])
+    expect(game.score()).toEqual(11)
+  })
+
+  it('ends the game after thirteen rounds', () => {
+    const game = new YahtzeeGame();
+    for (let i = 0; i < 13; i++) {
+      game.roll([1,1,1,1,1])
+    }
+    expect(game.score()).toEqual(5*13)
+    game.roll([1,1,1,1,1])
+    expect(game.score()).toEqual(5*13)
+  })
 })
--- src/YahtzeeGame.ts
+++ src/YahtzeeGame.ts
@@ -1,12 +1,27 @@
 export class YahtzeeGame {
+
+  runningScore: number;
+  round: number;
+
   constructor() {
+    this.round = 1;
+    this.runningScore = 0;
   }
 
-  roll (): void {
+  roll (roll: number[]): void {
+    if (this.round > 13) {
+      return;
+    }
 
+    this.runningScore += sum(roll)
+    this.round += 1
   }
 
   score (): number {
-    return 0
+    return this.runningScore
   }
+}
+
+const sum = (roll: number[]): number => {
+  return roll.reduce((acc, cur) => acc+cur, 0)
 }
