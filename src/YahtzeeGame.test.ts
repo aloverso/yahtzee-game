aab09755951af42978ae06697a66611674f1bfa6
Case Walker <case.b.walker@gmail.com>
2024-01-19 11:23:16 -0500

    Start working on rolls

--- src/YahtzeeGame.test.ts
+++ src/YahtzeeGame.test.ts
@@ -5,3 +5,11 @@ describe('Yahtzee Game', () => {
     expect(new YahtzeeGame().score()).toEqual(0)
   })
 })
+
+describe("Roll", () => {
+  it("increases the score always on the first roll", () => {
+    const yahtzee = new YahtzeeGame();
+    yahtzee.roll({ 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 5 });
+    expect(yahtzee.score() > 0);
+  })
+})
--- src/YahtzeeGame.ts
+++ src/YahtzeeGame.ts
@@ -1,8 +1,28 @@
+
+/*
+
+What to do:
+ * data structure of a roll
+ * data structure of running score entries
+ * what scores are possible with the current roll
+ * how to determine what categories can be picked for a certain roll
+ * figure out how we want to choose which category to go with
+ */
+
+interface Roll {
+  ones: number;
+  twos: number;
+  threes: number;
+  fours: number;
+  fives: number;
+  sixes: number;
+}
+
 export class YahtzeeGame {
   constructor() {
   }
 
-  roll (): void {
+  roll (roll: Roll): void {
 
   }
 
